0b6ebb3e041f261b0d273526b7a32eff01768b33
Rumburaq2 <93189782+Rumburaq2@users.noreply.github.com>
2023-12-17 14:53:57 +0100

    Delete djangoProject directory

--- djangoProject/products/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from .models import Product
-def porduct_detail_view(request, id):
-    obj=Product.objects.all(id=1)
-    return render(request, "product/detail.html", context={"title":obj.title, "decription":obj.decription})
--- djangoProject/testapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here. //request handler
-
-from django.shortcuts import render
-from django.http import HttpResponse
-def say_hello(request):
-    #return HttpResponse("hello world")
-    return render(request, 'hello.html', {'name': 'pero'})
